f6e90b1c56c5e1da6704314312ccaf6b5c6171bf
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-04-21 01:14:05 +0500

    Changed

--- script.js
+++ script.js
@@ -3,7 +3,14 @@
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
 
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEl.classList.add('hidden');
+btnRoll.addEventListener('click', function(){
+  const dice = Math.trunc(Math.random()*6)+1;
+  diceEl.classList.remove('.hidden');
+  diceEl.src = `../images/dice-${dice}.png`;
+  console.log(`${dice}`);
+})
