531c4bc211a8b761a8f72ea30c186dc8bbc508eb
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-04-21 20:43:52 +0500

    Changed

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
+const current0EL = document.querySelector('#current--0');
+const current1EL = document.querySelector('#current--1');
 
+const scores = [0, 0];
+let activePLayer = 0;
+let currentScore = 0;
 score0EL.textContent = 0;
 score1EL.textContent = 0;
+
 diceEl.classList.add('hidden');
 btnRoll.addEventListener('click', function(){
   const dice = Math.trunc(Math.random()*6)+1;
-  diceEl.classList.remove('.hidden');
+  diceEl.classList.remove('hidden');
   diceEl.src = `../images/dice-${dice}.png`;
-  console.log(`${dice}`);
-})
+
+if (dice !== 1) {
+  currentScore+=dice;
+  document.getElementById(`current--${activePLayer}`).textContent = currentScore;
+} else {
+  currentScore = 0;
+  activePLayer = activePLayer === 0 ? 1 : 0;
+  document.querySelector(`current--${activePLayer}`).textContent = currentScore;
+}})
