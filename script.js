ff048b329ba01af730a7463b701817bbcc2a10cf
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-04-26 18:49:23 +0500

    Changed

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 const current0EL = document.querySelector('#current--0');
 const current1EL = document.querySelector('#current--1');
 score0EL.textContent = 0;
@@ -24,7 +27,12 @@ if (dice !== 1) {
   currentScore+=dice;
   document.getElementById(`current--${activePLayer}`).textContent = currentScore;
 } else {
-  document.querySelector(`current--${activePLayer}`).textContent = 0;
-  currentScore = 0;
+  document.getElementById(`current--${activePLayer}`).textContent = 0;
+  currentScore=0;
   activePLayer = activePLayer === 0 ? 1 : 0;
+  player0EL.classList.toggle('player--active');
+  player1EL.classList.toggle('player--active');
 }})
+btnHold.addEventListener('click', function(){
+  
+})
